756f7ad8b8edae276f4dda7343bf41837ae05587
ciere <ciere@BPC_WINDOWS>
2025-11-18 18:45:31 +0100

    Add project files.

---
+++ MultiExpensesAPI/Models/Base/Entity.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiExpensesAPI.Models.Base
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime LastUpdatedAt { get; set; }
+    }
+}
---
+++ MultiExpensesAPI/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using MultiExpensesAPI.Models.Base;
+
+namespace MultiExpensesAPI.Models
+{
+    public class User : Entity
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
