9053b41e2eb2fbfa75c75a1cdee6309f6cea7192
bciereszynski <ciereszynskibp@gmail.com>
2025-11-18 19:58:16 +0100

    ME-1: Add transaction controller

---
+++ MultiExpensesAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiExpensesAPI.Data;
+using MultiExpensesAPI.Dtos;
+using MultiExpensesAPI.Models;
+
+namespace MultiExpensesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet("All")]
+        public IActionResult GetAll()
+        {
+            var allTransactions = context.Transactions.ToList();
+            return Ok(allTransactions);
+        }
+
+        [HttpGet("Details/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var transactionDb = context.Transactions.FirstOrDefault(n => n.Id == id);
+            if (transactionDb == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(transactionDb);
+        }
+
+        [HttpPut("Update/{id}")]
+        public IActionResult Update(int id, [FromBody] PostTransactionDto transactionDto)
+        {
+            var transactionDb = context.Transactions.FirstOrDefault(n => n.Id == id);
+            if (transactionDb == null)
+            {
+                return NotFound();
+            }
+
+            transactionDb.Type = transactionDto.Type;
+            transactionDb.Amount = transactionDto.Amount;
+            transactionDb.Category = transactionDto.Category;
+            transactionDb.LastUpdatedAt = DateTime.UtcNow;
+
+            context.Transactions.Update(transactionDb);
+            context.SaveChanges();
+            return Ok(transactionDb);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var transactionDb = context.Transactions.FirstOrDefault(n => n.Id == id);
+            if (transactionDb == null)
+            {
+                return NotFound();
+            }
+            context.Transactions.Remove(transactionDb);
+            context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost("Create")]
+        public IActionResult Create([FromBody] PostTransactionDto transactionDto)
+        {
+            var newTransaction = new Transaction
+            {
+                Type = transactionDto.Type,
+                Amount = transactionDto.Amount,
+                Category = transactionDto.Category,
+                Description = transactionDto.Description,
+                CreatedAt = DateTime.UtcNow,
+                LastUpdatedAt = DateTime.UtcNow
+            };
+            context.Transactions.Add(newTransaction);
+            context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
---
+++ MultiExpensesAPI/Dtos/PostTransactionDto.cs
@@ -0,0 +1,16 @@
+﻿using MultiExpensesAPI.Models.Base;
+
+namespace MultiExpensesAPI.Dtos
+{
+    public class PostTransactionDto
+    {
+        public string Type { get; set; }
+
+        public double Amount { get; set; }
+
+        public string Category { get; set; }
+
+        public string Description { get; set; }
+    }
+
+}
